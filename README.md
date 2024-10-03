85d81d79db6cf1fbc6704ec9f7374733358ec42b
Pedro Portales <pedroportales@riseup.net>
2024-10-02 21:39:04 -0300

    add update and delete

--- README.md
+++ README.md
@@ -23,6 +23,31 @@ npm i
 * 5xx -> Server error
 ```
 
+# Tipos de Requisição
+
+## Query Params (GET)
+* Consultar;
+* Exemplos:
+* * `servidor.com/usuarios?estado=bahia&cidade=salvador`
+* * `servidor.com/series?tipo=comedia&streaming=netflix`
+
+## Route Params (GET / PUT / DELETE)
+* Buscar, deletar ou editar algo específico
+* Exemplos:
+* * get `servidor.com/usuarios/22`
+* * put `servidor.com/usuarios/22`
+* * delete `servidor.com/usuarios/22`
+
+## Body Params (POST / PUT)
+* Exemplo:
+```json
+{
+  "nome": "Rodolfo",
+  "idade": 31,
+  "email": "rodolfo@email.com"
+}
+```
+
 # Prisma
 ```sh
 npm install prisma --save-dev
@@ -30,7 +55,7 @@ npx prisma init
 ```
 
 Preencha o .env de acordo com a [documentação](https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/mongodb-node-mongodb)
-```
+```.env
 DATABASE_URL="mongodb+srv://test:test@cluster0.ns1yp.mongodb.net/myFirstDatabase
 ```
 
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"; // Prisma
 
 const app = express();
-const prisma = new PrismaClient();
+const prisma = new PrismaClient(); // Prisma
 
 app.use(express.json());
 
-//const users = [];
+//const users = []; // Comentar se for usar Prisma
 
 // Criar usuário
 app.post('/users', async (req,res) => {
     //users.push(req.body);
-    await prisma.user.create({
+    await prisma.user.create({ // Prisma
         data: {
             email: req.body.email,
             name: req.body.name,
@@ -24,9 +24,48 @@ app.post('/users', async (req,res) => {
 
 // Consultar usuário
 app.get('/users', async (req,res) => {
-    const users = await prisma.user.findMany();
+    let users = [];
+
+    if(req.query){
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name
+            }
+        }); // Prisma
+    } else {
+        users = await prisma.users.findMany();
+    }
     
     res.status(200).json(users);
 });
+
+// Editar usuário
+app.put('/users/:id', async (req,res) => {
+    //users.push(req.body);
+    await prisma.user.update({ // Prisma
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    });
+
+    res.status(201).json(req.body);
+})
+
+// Deletar usuário
+app.delete('/users/:id', async (req,res) => {
+    //users.push(req.body);
+    await prisma.user.delete({ // Prisma
+        where: {
+            id: req.params.id
+        }
+    });
+
+    res.status(200).json({message: 'Usuário deletado com Sucesso!'});
+})
 
 app.listen(8081);
